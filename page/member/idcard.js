7136814b14a04fd5cffcba4157db9ec633eef7f9
黄鹏飞 <641655770@qq.com>
2019-04-08 16:10:07 +0800

    更新了‘身份证未上传’提示

--- page/member/idcard.js
+++ page/member/idcard.js
@@ -9,8 +9,12 @@ layui.use(['form', 'layer'], function () {
             type: "GET",
             success: function (result) {
                 if (result.code === 0) {
-                    $(".id_front").attr("src", result.data.idFront);  //身份证
-                    $(".id_back").attr("src", result.data.idBack);  //身份证
+                    if (!result.data.idFront && !result.data.idBack) {
+                        layer.msg("此用户身份证未上传");
+                    } else {
+                        $(".id_front").attr("src", result.data.idFront);  //身份证
+                        $(".id_back").attr("src", result.data.idBack);  //身份证
+                    }
                 } else {
                     layer.msg(result.exception, {icon: 7, anim: 6});
                 }
